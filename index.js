a8e11229fd4c161ae4e5f60a371e4c599822dc7f
SandraMTJ <sandra.tabares.j@gmail.com>
2023-02-22 23:12:11 -0500

    Updates

---
+++ index.js
@@ -0,0 +1,73 @@
+/* eslint-disable no-console */
+import validator from './validator.js';
+
+//En cuanto usuario da click a boton se activa función validate
+function validate() {
+    
+  const numTarjeta = document.getElementById('numTarjeta').value;//Variable que llama valor id del input 'numTarjeta'
+  const valid = validator.isValid(numTarjeta);// Se crea variable 'valid' que llama al método 'isValid' de objeto validador, para pasar # de tarjeta obtenido y devolver valor booleano
+  
+
+  //Se crea condicional, si variable 'valid' es true, se ejecuta acción o sino ('else')
+  if (valid){
+    document.getElementById('mensajeValido').innerHTML = 'Tarjeta válida';
+  } else {
+    document.getElementById('mensajeValido').innerHTML = 'Número de tarjeta inválido';
+  }}
+
+
+  
+// Obtener referencia al botón y al Mensaje de resultado
+const btnEnmascaramiento = document.getElementById('btnValidar'); //Variable 'btnEnmascaramiento' para llamar id de boton ('btnValidar')
+const numTarjetaInput = document.getElementById('numTarjeta');
+const resultado = document.getElementById('resultado'); //Variable 'resultado' para llamar id de elemento ('resultado')
+  
+const btnValidar = document.getElementById('btnValidar'); //Variable 'btnValidar' para llamar id de boton ('btnValidar')
+btnValidar.addEventListener('click', validate); //Se agrega evento al boton para que al dar click active función 'validate'
+  
+/*Se agrega evento al boton con variable creada 'btnEnmascaramiento'para que al dar click active función especifica, se crea variable 'numEnmascarado' para llamar 
+al método 'maskify' de objeto 'validator' y ejecuta la condicional*/
+
+btnEnmascaramiento.addEventListener('click', function () { 
+  const numTarjeta = numTarjetaInput.value;
+  //Se llama la función maskify para ocultar los caracteres del número de tarjeta, excepto los últimos cuatro, El resultado se asigna a la variable numEnmascarado.
+  const numEnmascarado = validator.maskify(numTarjeta); 
+  if (numEnmascarado === 'Número inválido') {
+    resultado.innerHTML = 'Número inválido';
+  } else {
+    resultado.innerHTML = numEnmascarado;
+  }
+});
+
+//Función para que solo permita números y no letras o caracteres especiales
+const campoInput = document.getElementById("numTarjeta");
+const mensajeError = document.getElementById("mensajeError");
+
+campoInput.addEventListener("input", () => { //El evento "input" se dispara cada vez que el usuario ingresa texto en el campo de entrada
+  if (campoInput.validity.patternMismatch) { //Se verifica si el valor actual del campo de entrada no coincide con el patrón especificado en el atributo 'pattern' del campo de entrada. En este caso, el patrón debe permitir solo caracteres numéricos. Si el valor actual del campo de entrada no coincide con el patrón, la condición del if se evalúa como true
+    mensajeError.textContent = "Solo se permiten números";
+  } else {
+    mensajeError.textContent = "";
+  } 
+
+
+  
+
+});
+  
+function validar() {
+  const campoInput = document.getElementById("numTarjeta").value;
+  const mensajeError = document.getElementById("mensajeError");
+  if (campoInput == "") {
+    document.getElementById('mensajeError').innerHTML = 'Por favor, ingrese número a validar.';
+    return false;
+  } else {
+    mensajeError.innerHTML = "";
+    return true;
+  }
+}
+
+
+
+
+console.log(validator);
---
+++ validator.js
@@ -0,0 +1,43 @@
+const validator = {
+  isValid(numTarjeta) {
+    {
+      const reversar = numTarjeta.toString().split("").reverse(); //Se convierte a cadena de caracteres, se invierte y se separa
+      let total = 0; // Calcula la suma de los dígitos de la tarjeta de crédito y se almacena en variable
+
+    
+      //Bucle del Algoritmo de Luhn
+      for (let i = 0; i < reversar.length; i++) {  //'reversar.length' Captura la cantidad de digitos del número ingresado
+        let num = parseInt(reversar[i]); // Variable 'num' para almacenar conversión de digito a número entero con 'parseInt'
+        if (i % 2 === 1) { // Verifica si el digito es número impar para continuar con la condicional if
+          num *= 2; // Si el digito es impar, se multiplica valor de 'num'por 2
+          if (num > 9) {
+            num -= 9;// Si el valor de 'num' despues de la multiplicación es mayor que 9, se resta 9
+          }
+        }
+        total += num; // El valor de 'num' luego de las operaciones se agrega a 'total' que es la suma de los digitos procesados en el bucle
+      }
+      if (total % 10 === 0) { // Se verifica si 'total' es divisible por 10 sin dejar residuo
+        return true; // Si es multiplo de 10 el número es válido (true)
+      } else {
+        return false; // Si no es multiplo de 10 el número es inválido (false)
+      }
+    }
+  },
+  //Ocultar los dígitos del número menos los últimos 4
+  maskify(numTarjeta) { //La función maskify toma el argumento 'numTarjeta'
+    
+    let numEnmascarado = ""; //Se toma variable 'numEnmascarado' como cadena de texto vacía, que se usará para almacenar los primeros dígitos del número ingresado
+    const cuatroDigitos = numTarjeta.slice(-4); //Se extraen los últimos cuatro dígitos con el método slice(-4) devolviendo los ultimos 4 caracteres 
+    
+    //Se utiliza un bucle 'for' para recorrer los dígitos de la tarjeta de crédito desde el primer dígito hasta el cuarto dígito desde el final. En cada iteración del bucle, se añade un carácter "#" a la cadena maskedNumber.
+    for (let i = 0; i < numTarjeta.length - 4; i++) {
+      i < numTarjeta.length - 4 
+      numEnmascarado += "#"; 
+    }
+    return numEnmascarado + cuatroDigitos; //Se concatenan los números ocultos y los 4 que quedan visibles
+  },
+
+};
+
+
+export default validator;
